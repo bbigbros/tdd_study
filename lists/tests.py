1b96e7320d9f50edb4fea71830c231d3462c288e
JayIm <steveim1201@gmail.com>
2017-09-01 17:32:23 +0900

    템플릿뷰 이용하도록 리펙토링 작업1

--- lists/tests.py
+++ lists/tests.py
@@ -10,16 +10,11 @@ class SmokeTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
-        print("resolve: " + str(found))
-        print("home_page: " + str(home_page))
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        print("-" * 30)
-        print(response.content)
-        print("-" * 30)
 
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
